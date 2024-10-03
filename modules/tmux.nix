d8ff7a6e7602b464711858b27cdd1c6eef3d7900
mateoroldos <mateoroldos19@gmail.com>
2024-10-03 11:26:16 -0300

    17   2024-10-03 00:00:14   (current)

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -1,9 +1,13 @@
-_: {
+{pkgs, ...}: {
   programs.tmux = {
     enable = true;
     # enableSensible = true;
     # enableVim = true;
     # extraConfig = ''
     # '';
+
+    plugins = with pkgs; [
+      tmuxPlugins.better-mouse-mode
+    ];
   };
 }
