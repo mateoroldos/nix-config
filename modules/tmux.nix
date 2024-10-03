8f802fea03123de8ef4bb83a12fca18a68c718c2
mateoroldos <mateoroldos19@gmail.com>
2024-10-03 12:41:50 -0300

    23   2024-10-03 12:35:50   (current)

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -10,5 +10,16 @@
       tmuxPlugins.better-mouse-mode
       tmuxPlugins.catppuccin
     ];
+
+    extraConfig = ''
+      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
+      set -g default-terminal "xterm-256color"
+      set -ga terminal-overrides ",*256col*:Tc"
+      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
+      set-environment -g COLORTERM "truecolor"
+
+      # Mouse works as expected
+      set-option -g mouse on
+    '';
   };
 }
