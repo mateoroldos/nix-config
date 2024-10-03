fba6f30595443e7853373145a16263a3443b41e5
mateoroldos <mateoroldos19@gmail.com>
2024-10-03 12:53:36 -0300

    24   2024-10-03 12:42:09   (current)

--- modules/tmux.nix
+++ modules/tmux.nix
@@ -3,22 +3,41 @@
     enable = true;
     # enableSensible = true;
     # enableVim = true;
-    # extraConfig = ''
-    # '';
+
+    terminal = "kitty";
 
     plugins = with pkgs; [
       tmuxPlugins.better-mouse-mode
-      tmuxPlugins.catppuccin
+      {
+        plugin = tmuxPlugins.catppuccin;
+        extraConfig = ''
+          set-option -g status-position top
+          set -g @catppuccin_window_left_separator ""
+          set -g @catppuccin_window_right_separator " "
+          set -g @catppuccin_window_middle_separator " █"
+          set -g @catppuccin_window_number_position "right"
+
+          set -g @catppuccin_window_default_fill "number"
+          set -g @catppuccin_window_default_text "#W"
+
+          set -g @catppuccin_window_current_fill "number"
+          set -g @catppuccin_window_current_text "#W"
+
+          set -g @catppuccin_status_modules_right ""
+          set -g @catppuccin_status_left_separator  " "
+          set -g @catppuccin_status_right_separator ""
+          set -g @catppuccin_status_fill "icon"
+          set -g @catppuccin_status_connect_separator "no"
+
+          set -g @catppuccin_directory_text "#{pane_current_path}"
+          set -g @catppuccin_status_background "default"
+          set-option -g default-terminal "screen-256color"
+        '';
+      }
     ];
 
     extraConfig = ''
-      # https://old.reddit.com/r/tmux/comments/mesrci/tmux_2_doesnt_seem_to_use_256_colors/
-      set -g default-terminal "xterm-256color"
-      set -ga terminal-overrides ",*256col*:Tc"
-      set -ga terminal-overrides '*:Ss=\E[%p1%d q:Se=\E[ q'
-      set-environment -g COLORTERM "truecolor"
-
-      # Mouse works as expected
+      # Mouse works as expecte
       set-option -g mouse on
     '';
   };
