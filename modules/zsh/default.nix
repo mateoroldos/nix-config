7d3e5f0dafca5bd6fdb72f84aeebb62d46d3d483
mateoroldos <mateoroldos19@gmail.com>
2024-10-03 19:26:57 -0300

    28   2024-10-03 19:23:26   (current)

--- modules/zsh/default.nix
+++ modules/zsh/default.nix
@@ -27,6 +27,7 @@
       reload = "source ~/.config/zsh/.zshrc";
       zed = "open -a /Applications/Zed.app -n";
       fnvim = "nvim $(fzf -m --preview='bat --color=always {}')";
+      fzfb = "fzf -m --preview='bat --color=always'";
     };
 
     initExtraBeforeCompInit = ''
